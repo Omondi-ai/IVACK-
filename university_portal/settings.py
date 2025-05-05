6a137d42c47402de2e1bc91f343ce3753360fb7b
Omondi-ai <odhiamboderrick01@gmail.com>
2025-05-05 22:43:06 +0300

    'ivac_university'

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ljedl-b7gta$+n#e%fx1_z08t7o8tbrv9(ljs0c3!(j)l5u6tb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
