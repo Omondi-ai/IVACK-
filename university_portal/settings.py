2f3c3fc61d6bd1b826a1bc565d951ea5ffdad92b
Omondi-ai <odhiamboderrick01@gmail.com>
2025-05-07 22:23:47 +0300

    'deploying_on_railway'

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -5,7 +5,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
