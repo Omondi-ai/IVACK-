441adfc20465e072a567bbd37296ae5223c217a8
Omondi-ai <odhiamboderrick01gmail.com>
2025-06-22 22:24:56 +0300

    'tryingdocker_FIXED_programming_error_and_migrating'

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,6 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn server..."
-exec gunicorn university_portal.wsgi:application --bind 0.0.0.0:$PORT
+CMD python manage.py migrate --noinput && gunicorn university_portal.wsgi:application --bind 0.0.0.0:$PORT
+
+
